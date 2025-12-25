6e515d32740162623258f4d30fdaea20e02fb482
Bruno Strano <bruno_strano@hotmail.com>
2025-12-25 12:56:04 -0300

    🔧 Update .gitignore to include environment files

---
+++ src/modules/auth/strategies/life-gamification-jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { EnvironmentService } from '../../../configs/environment/environment.service';
+import {
+  lookupPlatformName,
+  PlatformEnum,
+} from '../../../shared/constants/PlatformEnum';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { TokenPayloadInterface } from '../types/TokenPayloadInterface';
+import { PassportStrategy } from '@nestjs/passport';
+
+@Injectable()
+export class FinancialJwtStrategy extends PassportStrategy(
+  Strategy,
+  lookupPlatformName(PlatformEnum.LIFE_GAMIFICATION),
+) {
+  constructor(private readonly environmentService: EnvironmentService) {
+    super({
+      issuer: `Stralom-${lookupPlatformName(PlatformEnum.AUTHENTICATION)}`,
+      audience: `Stralom-${lookupPlatformName(PlatformEnum.LIFE_GAMIFICATION)}`,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: environmentService.accessTokenJwtConfig(
+        PlatformEnum.LIFE_GAMIFICATION,
+      ).secret,
+    });
+  }
+
+  async validate(payload: TokenPayloadInterface) {
+    return { email: payload.email, sub: payload.sub, name: payload.name };
+  }
+}
--- src/shared/constants/PlatformEnum.ts
+++ src/shared/constants/PlatformEnum.ts
@@ -4,6 +4,7 @@ export enum PlatformEnum {
   FINANCIAL = 1,
   KEYCHAIN = 2,
   AUTHENTICATION = 3,
+  LIFE_GAMIFICATION = 4,
 }
 
 export const lookupPlatformName = (platform: PlatformEnum) => {
@@ -14,6 +15,8 @@ export const lookupPlatformName = (platform: PlatformEnum) => {
       return 'KEYCHAIN';
     case PlatformEnum.AUTHENTICATION:
       return 'AUTH';
+    case PlatformEnum.LIFE_GAMIFICATION:
+      return 'LIFE_GAMIFICATION';
     default:
       throw new InternalServerErrorException(`Platform ${platform} invalid`);
   }
